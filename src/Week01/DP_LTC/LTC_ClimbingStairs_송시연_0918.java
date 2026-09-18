3e174ac3343eacfa7f21f2fc47db2857e8bcd790
dustle <pwdustle@gmail.com>
2026-09-19 01:41:00 +0900

    feat: 0918 풀이

---
+++ src/Week01/DP_LTC/LTC_ClimbingStairs_송시연_0918.java
@@ -0,0 +1,15 @@
+package Week01.DP_LTC;
+
+public class LTC_ClimbingStairs_송시연_0918 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
